d0a1c0ff7438c4c30b70241f1cce237d8db73f40
speeldesign <m.hokamura@speel.design>
2021-02-19 21:47:12 +0900

    udemy app 1-2

--- mySwiftApp/ViewController.swift
+++ mySwiftApp/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter = 0
+    @IBOutlet weak var countLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countLabel.text = String(counter)
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func up(_ sender: Any) {
+        countUp()
+        changeColor()
+    }
+    @IBAction func down(_ sender: Any) {
+        countDown()
+        changeColor()
+    }
+    
+    func countUp() {
+        counter = counter + 1
+        countLabel.text = String(counter)
+    }
+    func countDown() {
+        counter = counter - 1
+        countLabel.text = String(counter)
+    }
+    func changeColor() {
+        if counter < 10 {
+            countLabel.textColor = .black
+        }
+        if counter >= 10 {
+            countLabel.textColor = .red
+        }
+    }
 
+    
 }
 
